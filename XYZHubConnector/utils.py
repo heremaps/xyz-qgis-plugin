ab037dea35ae625652eda0d9fe46bc7838a2131a
minff <16268924+minff@users.noreply.github.com>
2019-03-11 11:21:03 +0100

    move config file to qgis user folder (persistent across plugin version)
    Signed-off-by: minff <16268924+minff@users.noreply.github.com>

--- XYZHubConnector/utils.py
+++ XYZHubConnector/utils.py
@@ -30,6 +30,7 @@ def make_unique_full_path(ext="json"):
     # return os.path.join(config.TMP_DIR, "%s.%s"%("test",ext))
 def clear_cache():
     files = [os.path.join(config.TMP_DIR, f) for f in os.listdir(config.TMP_DIR)]
+    files.append(config.LOG_FILE)
     for f in files:
         try: os.remove(f)
         except OSError: pass # files in used
