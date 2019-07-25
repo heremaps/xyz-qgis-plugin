057ca2dc830926153bd6d47ee79656464bf0839a
minff <16268924+minff@users.noreply.github.com>
2019-07-25 14:02:35 +0200

    archive log file when it gets big

--- XYZHubConnector/utils.py
+++ XYZHubConnector/utils.py
@@ -8,10 +8,13 @@
 #
 ###############################################################################
 
-from qgis.PyQt.uic import loadUiType
-from . import config
 import os
 import time
+import shutil
+import gzip
+
+from qgis.PyQt.uic import loadUiType
+from . import config
 
 def disconnect_silent(signal):
     ok = True
@@ -35,3 +38,18 @@ def clear_cache():
     for f in files:
         try: os.remove(f)
         except OSError: pass # files in used
+
+def archive_log_file():
+    if not os.path.exists(config.LOG_FILE): return
+    threshold = 5*1024*1024
+    if os.path.getsize(config.LOG_FILE) < threshold: return
+    base,_ = os.path.split(config.LOG_FILE)
+    idx = len([s for s in os.listdir(base) if s.endswith(".gz")])
+    archive_path = os.path.join(base, "qgis.%s.log.gz"%idx)
+    with open(config.LOG_FILE, 'rb') as f_in:
+        with gzip.open(archive_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    with open(config.LOG_FILE, 'w') as f_in:
+        pass
+
+archive_log_file()
